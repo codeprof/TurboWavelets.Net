15ed20a1fe902a5bee124a0bece45fc7a3ef9032
Stefan Moebius <mail4stefm@gmail.com>
2016-04-18 09:25:20 +0200

    Update generateDocumentationAndDeploy.sh

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -107,7 +107,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # Force push to the remote gh-pages branch.
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
-    git push origin gh-pages --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
